73b5c9cac3314b4c5c863c2bf4a654c71f9ed4cc
Vivek T Ramamoorthy <vivektramamoorthy@gmail.com>
2022-04-04 03:03:58 +0200

    some updates to slope tracer

--- js/slope_tracer.js
+++ js/slope_tracer.js
@@ -11,45 +11,54 @@ let fun = x=> pow(x,2);
 let derivative = x=>x*2;
 let prob = "y=x"
 let derivativeString = "y=1"
+var functionPlotHandle, derivativePlotHandle
+var xlimits=[0,1], ylimits=[0,1]
+
+slopeTracerProblem()
 
 function slopeTracerProblem(prob="y=x"){
     switch (prob) {
         case "y=x":
-         fun = x=> x;
-         derivative = x=>1;
-         derivativeString = "y=1"
+        fun = x=> x;
+        derivative = x=>1;
+        derivativeString = "y=1";
+        xlimits=[-10,10]
+        ylimits=[-10,10]
         break;
-    
+        
         case "y=x^2":
-            fun = x=> x**2;
-            derivative = x=>2*x;
-            derivativeString = "y=2x"
-            break;
-    
+        fun = x=> x**2;
+        derivative = x=>2*x;
+        derivativeString = "y=2x"
+        xlimits=[-4,4]
+        ylimits=[-1,17]
+        break;
+        
         case "y=cosx":
-            fun = x=> Math.cos(x);
-            derivative = x=>Math.sin(x);
-            derivativeString = "y=sin(x)"
-            break;
-    
-        case "y=expx":
-            
-            break;
-    
+        fun = x=> Math.cos(x);
+        derivative = x=>Math.sin(x);
+        derivativeString = "y=sin(x)"
+        xlimits=[-Math.PI,Math.PI]
+        ylimits=[-1.5,1.5]
+        break;
+        
+        
         default:
-            break;
+        break;
     }
-
+    slope_tracer_setup()
+    
 }
 
-
-let x = range(-10,.1,10);
-let y = map(fun,x);
-let functionPlotHandle = figure("slope-tracer-function-canvas")
-plot(x,y,'padding',30,'xlabel','','ylabel','','title','y=x^2')
-let dydx = map(derivative,x);
-let derivativePlotHandle = figure("slope-tracer-derivative-canvas")
-plot(x,dydx,'axis','fixed','xlim',[-10,10],'ylim',[-20,20],'padding',40,'xlabel','','ylabel','','title',derivativeString)
+function slope_tracer_setup(){
+    let x = range(xlimits[0],.1,xlimits[1]);
+    let y = map(fun,x);
+    functionPlotHandle = figure("slope-tracer-function-canvas")
+    plot(x,y,'padding',30,'xlabel','','ylabel','','title','y=x^2')
+    let dydx = map(derivative,x);
+    derivativePlotHandle = figure("slope-tracer-derivative-canvas")
+    plot(x,dydx,'axis','fixed','xlim',xlimits,'ylim',ylimits,'padding',40,'xlabel','','ylabel','','title',derivativeString)
+}
 
 
 
@@ -62,7 +71,7 @@ var deltaX = 1;
 function slopeTracerCanvasUpdate(){
     slopeTracerFunctionCanvas.width = slopeTracerFunctionCanvas.clientWidth;
     slopeTracerFunctionCanvas.height = slopeTracerFunctionCanvas.clientHeight;
-
+    
     // update function plot and slope line
     functionPlotHandle.drawLines()
     let ptx = functionPlotHandle.pxToX(mouse.x)
@@ -73,16 +82,16 @@ function slopeTracerCanvasUpdate(){
     functionPlotHandle.drawLine({x:[ptx-deltaX,ptx+deltaX, ptx+deltaX],y:[pty1,pty1,pty2],color:"#f00"})
     functionPlotHandle.clearPadding()
     functionPlotHandle.drawAxes()
-
-
+    
+    
     // update derivative plot and height line
-
+    
     let ptdydx = map(derivative,ptx);
     derivativePlotHandle.draw()
     derivativePlotHandle.drawPoint(ptx,ptdydx,5,"#f00") // draw mouse point
     derivativePlotHandle.drawLine({x:[ptx,ptx],y:[0,ptdydx],color:"#f00"}) // draw mouse point
     derivativePlotHandle.drawLine({x:[-10,10],y:[0,0],color:"#000"}) // draw mouse point
-
+    
 }
 // slopeTracerCanvasUpdate()
 
