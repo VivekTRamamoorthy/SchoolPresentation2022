10b5ca5279c958561f3a2a6533fb1052d0da7b1f
Vivek T Ramamoorthy <vivektramamoorthy@gmail.com>
2022-04-08 08:11:10 +0200

    variable delta x in slope tracer

--- js/slope_tracer.js
+++ js/slope_tracer.js
@@ -1,18 +1,38 @@
-
-
-
-
+// Tracing the slope from mouse position point
 
 
 var slopeTracerFunctionCanvas=document.getElementById("slope-tracer-function-canvas");
 var slopeTracerDerivativeCanvas=document.getElementById("slope-tracer-derivative-canvas");
 
-let fun = x=> pow(x,2);
-let derivative = x=>x*2;
+let fun = x=> pow(x,2); // function 
+let derivative = x=>x*2; // its derivative
 let prob = "y=x"
 let derivativeString = "y=1"
 var functionPlotHandle, derivativePlotHandle
 var xlimits=[0,1], ylimits=[0,1]
+var deltaX=document.getElementById("")
+
+var deltaX = 1;
+var slopeTracerDeltaXElem = document.getElementById("slopeTracerDeltaXSlider")
+slopeTracerDeltaXElem.oninput=()=>{
+    deltaX = slopeTracerDeltaXElem.value*0.01;
+    console.log(deltaX);
+    let label = document.getElementById("slopeTracerDeltaXLabel")
+    label.innerText = "$\\Delta x$ = "+deltaX.toFixed(1);
+    LaTeXrender(label)
+    slopeTracerCanvasUpdate()
+}
+var mouse={x:0,y:0}
+
+
+slopeTracerFunctionCanvas.addEventListener("mousemove",(event)=>{
+    const rect = slopeTracerFunctionCanvas.getBoundingClientRect()
+    mouse.x=event.clientX - rect.left;
+    mouse.y=event.clientY - rect.top;
+    // console.log(event)
+    slopeTracerCanvasUpdate()
+})
+
 
 slopeTracerProblem()
 
@@ -21,7 +41,7 @@ function slopeTracerProblem(prob="y=x"){
         case "y=x":
         fun = x=> x;
         derivative = x=>1;
-        derivativeString = "y=1";
+        derivativeString = "dy/dx=1";
         xlimits=[-10,10]
         ylimits=[-10,10]
         break;
@@ -29,7 +49,7 @@ function slopeTracerProblem(prob="y=x"){
         case "y=x^2":
         fun = x=> x**2;
         derivative = x=>2*x;
-        derivativeString = "y=2x"
+        derivativeString = "dy/dx=2x"
         xlimits=[-4,4]
         ylimits=[-1,17]
         break;
@@ -37,7 +57,7 @@ function slopeTracerProblem(prob="y=x"){
         case "y=cosx":
         fun = x=> Math.cos(x);
         derivative = x=>Math.sin(x);
-        derivativeString = "y=sin(x)"
+        derivativeString = "dy/dx=sin(x)"
         xlimits=[-Math.PI,Math.PI]
         ylimits=[-1.5,1.5]
         break;
@@ -54,17 +74,17 @@ function slope_tracer_setup(){
     let x = range(xlimits[0],.1,xlimits[1]);
     let y = map(fun,x);
     functionPlotHandle = figure("slope-tracer-function-canvas")
-    plot(x,y,'padding',30,'xlabel','','ylabel','','title','y=x^2')
+    plot(x,y,'padding',60,'xlabel','','ylabel','','title','y=x^2')
     let dydx = map(derivative,x);
     derivativePlotHandle = figure("slope-tracer-derivative-canvas")
-    plot(x,dydx,'axis','fixed','xlim',xlimits,'ylim',ylimits,'padding',40,'xlabel','','ylabel','','title',derivativeString)
+    plot(x,dydx,'axis','fixed','xlim',xlimits,'ylim',ylimits,'padding',60,'xlabel','','ylabel','','title',derivativeString,'color','red')
 }
 
 
 
-var mouse={x:0,y:0}
 
-var deltaX = 1;
+
+
 
 
 
@@ -85,7 +105,6 @@ function slopeTracerCanvasUpdate(){
     
     
     // update derivative plot and height line
-    
     let ptdydx = map(derivative,ptx);
     derivativePlotHandle.draw()
     derivativePlotHandle.drawPoint(ptx,ptdydx,5,"#f00") // draw mouse point
@@ -93,16 +112,7 @@ function slopeTracerCanvasUpdate(){
     derivativePlotHandle.drawLine({x:[-10,10],y:[0,0],color:"#000"}) // draw mouse point
     
 }
-// slopeTracerCanvasUpdate()
 
-
-slopeTracerFunctionCanvas.addEventListener("mousemove",(event)=>{
-    const rect = slopeTracerFunctionCanvas.getBoundingClientRect()
-    mouse.x=event.clientX - rect.left;
-    mouse.y=event.clientY - rect.top;
-    // console.log(event)
-    slopeTracerCanvasUpdate()
-})
 
 
 
